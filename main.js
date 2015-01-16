82e059b2acb09bd88928f985c708cccc0f405b70
Kyle Petrovich <kylepetrovich@gmail.com>
2015-01-16 00:46:59 -0500

    a bit sprused up version to show the potential

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
-// Ask user for their age
-var age = prompt("What is your age?");
+var ageInput = document.getElementById("ageInput");
+ageInput.addEventListener("input", calculateAge);
 
-// prompt() returns a String, we need to change it into a Number so we can do math!
-age = parseInt(age, 10);
+calculateAge({target: {value: 0}});
+function calculateAge(event) {
+    age = parseInt(event.target.value, 10) || 0;
 
-// Constant numbers to help you out
-var heartbeatsPerYear = 42000000;
-var daysPerYear = 365;
-var gallonsPerYear = 52;
+    // Constant numbers to help you out
+    var heartbeatsPerYear = 42000000;
+    var daysPerYear = 365;
+    var gallonsPerYear = 52;
+    var cerealPerYear = 260;
+    var hoursPerWeek = 40;
+    var workWeeksPerYear = 49;
 
-// Calculate our "facts" to fill in
-var heartbeats = age * heartbeatsPerYear;
-var days = age * daysPerYear;
-var gallons = age * gallonsPerYear;
+    // Calculate our "facts" to fill in
+    var heartbeats = age * heartbeatsPerYear;
+    var days = age * daysPerYear;
+    var gallons = age * gallonsPerYear;
+    var cerealBowls = Math.max(0, age - 5) * cerealPerYear;
+    var workHours = 49 * 40 * Math.max(0, age - 22);
 
-// Select our answer elements from the HTML document
-var heartBeatsAnswer = document.getElementById("heartbeatAnswer");
-var daysAnswer = document.getElementById("daysAnswer");
-var waterAnswer = document.getElementById("waterAnswer");
+    // Select our answer elements from the HTML document
+    var heartBeatsAnswer = document.getElementById("heartbeatAnswer");
+    var daysAnswer = document.getElementById("daysAnswer");
+    var waterAnswer = document.getElementById("waterAnswer");
+    var cerealAnswer = document.getElementById("cerealAnswer");
+    var workAnswer = document.getElementById("workAnswer");
 
-// Fill in our answers by setting innerHTML
-heartBeatsAnswer.innerHTML = heartbeats + " heartbeats";
-daysAnswer.innerHTML = days + " days";
-waterAnswer.innerHTML = gallons + " gallons";
+    // Fill in our answers by setting innerHTML
+    heartBeatsAnswer.innerHTML = heartbeats;
+    daysAnswer.innerHTML = days;
+    waterAnswer.innerHTML = gallons;
+    cerealAnswer.innerHTML = cerealBowls;
+    workAnswer.innerHTML = workHours;
+}
+
